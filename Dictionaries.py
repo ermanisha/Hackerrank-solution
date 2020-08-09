bd0fc108d400955a7a13afa967bc3ef6a363231b
ermanisha <51367704+ermanisha@users.noreply.github.com>
2020-08-09 15:15:12 +0530

    Update Dictionaries.py

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,4 +1,4 @@
-import PyPDF2
+#DICTINARY PRACTICE PROBLEM USED TO FIND THE COUNT OF WORD IN TXT FILE AND RETURNS THE MAXIMUM COUNT
 fh=input("enter the file")
 fhand=open(fh)
 di=dict()
@@ -15,3 +15,44 @@ for k,v in di.items():
         large=v
 print(large)
     
+ # socket programming
+import socket
+mysock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+mysock.connect(("data.pr4e.org",80))
+cmd='GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+
+while True:
+    data=mysock.recv(512)
+    if len(data) <1:
+        break
+    print(data.decode(),end=" ")
+mysock.close()
+
+#dictionary assignment USED TO READ A FILE GET THE COUNT OF THE PERSON WHO HAS SEND THE MAXIMUM NUMBER OF EMAILS.
+name = input("Enter file:")
+if len(name) < 1 : name = "sample.txt"
+handle = open(name)
+di=dict()
+for line in handle:
+    line.strip
+    if line.startswith('From '):
+        wd=line.split()
+        di[wd[1]]=di.get(wd[1],0)+1
+        print(di[wd[1]])
+
+print(di)
+large=-1 
+word=None
+for k,v in di.items():
+    if v>large:
+        large=v
+        word=k
+print(word,large)
+        
+        
+        
+
+    
+
