0340016ce97fa3ec65760b7e54f12d08c7c8ac66
ermanisha <51367704+ermanisha@users.noreply.github.com>
2020-08-14 12:37:13 +0530

    Update intro.php

--- intro.php
+++ intro.php
@@ -10,3 +10,35 @@
 
 </body>
 </html>
+
+***************************
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP SYNTAX</title>
+</head>
+<body>
+	<?php
+	
+	$x=3;
+	$y=7.7;
+	echo $var1;
+	echo "<br>";
+	echo $x;
+	echo "<br>";
+	echo $y;
+	echo "<hr/>";
+
+	$sports="football";
+
+	echo "i like $sports !"."<br>";
+	echo "<hr />";
+
+
+	$x=8;
+	$y=9;
+	echo $x+$y;
+		?>
+
+</body>
+</html>
