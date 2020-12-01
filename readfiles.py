1a465073657e5d97d558c57ecf053a27df59b25c
ma23nisha <51367704+ma23nisha@users.noreply.github.com>
2020-12-01 23:27:24 +0530

    Update readfiles.py

--- readfiles.py
+++ readfiles.py
@@ -9,4 +9,32 @@ for line in fh:
             lst.append(word)
 lst.sort()
 print(lst)
+
+
     
+a{
+    display:block;
+    font-weight:bold;
+    color:#ffffff;
+    background-color: #0006CC;
+    width:200px;
+    text-align:center;
+    padding:4px;
+    text-decoration:none;
+    margin:0 auto;
+    margin-top:10px;
+}
+
+a:hover{
+    background: #B3B3B3;
+    color:#0006CC;
+}
+
+a:focus{
+    background:#B3B3B3;
+    color:#0006CC;
+}
+
+a:active{
+    border:3px solid #000000;
+}
